d612cc01f24ce50de36c76b0ad01c6db0862e4f0
chevtong <chevtongwingyan@gmail.com>
2023-03-19 17:18:07 +0100

    feat: add GarageCard and use in Parkings screen

---
+++ src/components/GarageCard.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {colors} from '../utils/colors';
+import openMap from 'react-native-open-maps';
+import {Card} from './Card';
+
+interface GarageCardProps {
+  /**
+   * Name of the parking garage
+   */
+  name: string;
+  /**
+   * Number of availability in percentage
+   */
+  availablityPercenatge: number;
+  /**
+   * Coordinate of the garage
+   */
+  coordinates: number[];
+}
+
+export const GarageCard = ({
+  name,
+  coordinates,
+  availablityPercenatge,
+}: GarageCardProps) => {
+  const openLocation = (latitude: number, longitude: number) => {
+    openMap({
+      provider: 'google',
+      latitude: latitude,
+      longitude: longitude,
+    });
+  };
+
+  return (
+    <Card
+      onPress={() => openLocation(coordinates[0], coordinates[1])}
+      title={name}
+      accessibilityHint="Press to open in Google Maps.">
+      <View style={styles.display}>
+        <Icon name={'local-parking'} size={16} color={colors.grey} />
+        <Text style={styles.text}>
+          Availability: {Math.trunc(availablityPercenatge)}%
+        </Text>
+      </View>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  display: {
+    flexDirection: 'row',
+    gap: 5,
+    paddingVertical: 5,
+  },
+  text: {
+    color: colors.grey,
+  },
+});
--- src/screens/Parkings.tsx
+++ src/screens/Parkings.tsx
@@ -1,13 +1,70 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import * as React from 'react';
+import {Text, View, FlatList, StyleSheet} from 'react-native';
+import {GarageCard} from '../components/GarageCard';
+import {colors} from '../utils/colors';
+import {ActivityIndicator} from 'react-native';
 import {useGarageData} from '../utils/hooks/useGarageData';
 
 export const Parkings = () => {
   const {data, isLoading} = useGarageData();
-  console.log('data:', data);
+
   return (
-    <View>
-      <Text>Parkings</Text>
+    <View
+      accessible={true}
+      accessibilityLabel="List of available parkings."
+      style={styles.screenContainer}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={({item}) => {
+            return (
+              <GarageCard
+                name={item.name}
+                coordinates={item.coordinates}
+                availablityPercenatge={item.availablePercentage}
+              />
+            );
+          }}
+          keyExtractor={item => item.id}
+          ListEmptyComponent={
+            <Text
+              style={styles.emptyText}
+              accessible={true}
+              accessibilityLabel="No Available Data">
+              No Data
+            </Text>
+          }
+          ListHeaderComponent={
+            <Text
+              style={styles.title}
+              accessible={true}
+              accessibilityLabel="Available Bikes:">
+              Available Parkings:
+            </Text>
+          }
+        />
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    padding: 10,
+  },
+  title: {
+    paddingVertical: 10,
+    fontSize: 20,
+    paddingHorizontal: 20,
+    fontWeight: '600',
+    color: colors.grey,
+  },
+  emptyText: {
+    color: colors.grey,
+    padding: 10,
+    textAlign: 'center',
+  },
+});
